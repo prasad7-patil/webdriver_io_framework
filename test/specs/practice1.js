b63c25137f8451518698e191e704f43a2b9ed829
prasad7-patil <shivpatil40@gmail.com>
2023-02-24 20:53:14 +0530

    fixed practice1

---
+++ test/specs/practice1.js
@@ -0,0 +1,8 @@
+describe("admin",async()=>{
+    it("login",async()=>{
+        await browser.url("http://www.google.com/")
+        await browser.maximizeWindow()
+        await browser.$('[name="q"]').setValue("alexnder supertramp")
+        
+    })
+})
